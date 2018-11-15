1a35b0d5eddbd19d85062b29db5eb470cec97fba
Google7 <18207156427@139.com>
2018-11-15 15:08:43 +0800

    two

--- test.js
+++ test.js
@@ -1,4 +1,25 @@
-﻿var n;
-for (let i = 2; i < 100; i+=2) {
-    console.log(i);    
+﻿var str = "d5r44va8wet4vbad8aebadhtyui4564pjmbn666";
+
+function findNum(str) {
+    var tmp = "";
+    var arr = [];
+    for (var i = 0; i < str.length; i++) {
+        var cur = str[i];
+        if (!isNaN(cur)) {
+            tmp += cur;
+        } else {
+            if (tmp) {
+                arr.push(tmp);
+                tmp = "";
+            }
+        }
+    }
+    if (tmp) {
+        arr.push(tmp);
+    }
+    return arr;
 }
+var obj = findNum(str);
+console.log(obj);
+console.log(str.charAt(0));
+console.log(str.charCodeAt(0));
